4eb2bb85f7e05babdd0481760a4fcf89b1e61df6
ShuangHan-0705 <69603535+ShuangHan-0705@users.noreply.github.com>
2021-06-17 23:22:51 -0400

    Addition test

---
+++ cypress/integration/Addition.spec.js
@@ -0,0 +1,8 @@
+context('Addition', () => {
+})
+
+describe('Add two numbers', () => {
+    it('.should() - make an assertion about the addition', () => {
+        cy.expect(10 + 15).to.equal(25)
+    })
+})
