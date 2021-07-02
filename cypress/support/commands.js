b22d275b0443dc2633c87f65158e2280b36449a0
ShuangHan-0705 <69603535+ShuangHan-0705@users.noreply.github.com>
2021-07-02 10:15:02 -0400

    Update with feedbacks

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -45,47 +45,64 @@ Cypress.Commands.add("addToCart", (num) => {
     number: num,
   });
 
-  var items = new Array(num);
+  //A complex way:
 
-  for (let i = 0; i < num; i++) {
-    if (i == 0) {
-      var random = Math.floor(Math.random() * 6) + 1;
-      items[i] = random;
-    } else {
-      var random = Math.floor(Math.random() * 6) + 1;
-      if (items.includes(random)) {
-        i--;
-        //break;
-      } else {
-        items[i] = random;
-      }
-    }
+  // var items = new Array(num);
+
+  // for (let i = 0; i < num; i++) {
+  //   if (i == 0) {
+  //     var random = Math.floor(Math.random() * 6) + 1;
+  //     items[i] = random;
+  //   } else {
+  //     var random = Math.floor(Math.random() * 6) + 1;
+  //     if (items.includes(random)) {
+  //       i--;
+  //       //break;
+  //     } else {
+  //       items[i] = random;
+  //     }
+  //   }
+  // }
+
+  // for (let j = 0; j < num; j++) {
+  //   switch (items[j].toString()) {
+  //     case "6":
+  //       cy.get(
+  //         "[data-test='add-to-cart-test.allthethings()-t-shirt-(red)']"
+  //       ).click();
+  //       break;
+  //     case "5":
+  //       cy.get("[data-test = add-to-cart-sauce-labs-bike-light]").click();
+  //       break;
+  //     case "4":
+  //       cy.get("[data-test = add-to-cart-sauce-labs-bolt-t-shirt]").click();
+  //       break;
+  //     case "3":
+  //       cy.get("[data-test = add-to-cart-sauce-labs-backpack]").click();
+  //       break;
+  //     case "2":
+  //       cy.get("[data-test = add-to-cart-sauce-labs-onesie]").click();
+  //       break;
+  //     case "1":
+  //       cy.get("[data-test = add-to-cart-sauce-labs-fleece-jacket]").click();
+  //       break;
+  //   }
+  //}
+
+  //A simpler way:
+
+  function clickAddToCartButton(number) {
+    if (number <= 0) return;
+    cy.get('button:contains("Add to cart")').then((addToCartBtns) => {
+      const length = Cypress.$(addToCartBtns).length;
+      cy.log("length is" + length);
+      const random = Math.floor(Math.random() * length);
+      cy.get(addToCartBtns).eq(random).click();
+      clickAddToCartButton(number - 1);
+    });
   }
 
-  for (let j = 0; j < num; j++) {
-    switch (items[j].toString()) {
-      case "6":
-        cy.get(
-          "[data-test='add-to-cart-test.allthethings()-t-shirt-(red)']"
-        ).click();
-        break;
-      case "5":
-        cy.get("[data-test = add-to-cart-sauce-labs-bike-light]").click();
-        break;
-      case "4":
-        cy.get("[data-test = add-to-cart-sauce-labs-bolt-t-shirt]").click();
-        break;
-      case "3":
-        cy.get("[data-test = add-to-cart-sauce-labs-backpack]").click();
-        break;
-      case "2":
-        cy.get("[data-test = add-to-cart-sauce-labs-onesie]").click();
-        break;
-      case "1":
-        cy.get("[data-test = add-to-cart-sauce-labs-fleece-jacket]").click();
-        break;
-    }
-  }
+  clickAddToCartButton(num);
 });
 
 Cypress.Commands.add("clearCart", () => {
@@ -93,9 +110,14 @@ Cypress.Commands.add("clearCart", () => {
     name: "clearCart",
   });
 
-  cy.get(".cart_item").then(($ci) => {
-    for (let i = 0; i < $ci.length; i++) {
-      cy.get("button").contains("Remove").click();
-    }
+  // cy.get(".cart_item").then(($ci) => {
+  //   for (let i = 0; i < $ci.length; i++) {
+  //     cy.get("button").contains("Remove").click();
+  //   }
+  // });
+
+  //Another way with each()
+  cy.get(".cart_item").each((cartitem) => {
+    cy.get(cartitem).contains("Remove").click();
   });
 });
