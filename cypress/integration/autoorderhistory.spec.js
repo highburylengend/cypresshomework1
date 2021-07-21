b17727250eed6e23bba132824e42af57e45c4679
highburylengend <86175168+highburylengend@users.noreply.github.com>
2021-07-21 12:49:26 +1200

    Update autoorderhistory.spec.js

--- cypress/integration/autoorderhistory.spec.js
+++ cypress/integration/autoorderhistory.spec.js
@@ -13,11 +13,18 @@ describe("Test order history page", () => {
     cy.get("tbody>tr").should("have.length", 2);
 
     //Why the following call is incorrect???
+    // 把orderHistoryPage.getOrders()的实现改成这样就好了
+    //  getOrders() {
+    //    return cy.get('tbody>tr');
+    //  }
     //orderHistoryPage.getOrders().should("have.length", 2);
   });
 
   //This case failed because the system signed out after clicking on the "Go back to account", don't know why???
+  // 我调查了下，可能和cypress在每个case的一开始清理cookies有关，详见这个文档https://docs.cypress.io/api/commands/clearcookies
+  // 我把该文件第六行的before改成beforeEach，并且在你下面这个case里加一句话，这个问题就没了
   it("Go back to account page", () => {
+    //accountPage.goOrderHistory();
     orderHistoryPage.goBackToAccount();
     cy.contains("order history and details").should("be.visible");
   });
