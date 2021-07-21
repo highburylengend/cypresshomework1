9336e1dd5294a2e060a54db58b993fc6a8ab72e5
highburylengend <86175168+highburylengend@users.noreply.github.com>
2021-07-21 12:52:39 +1200

    Update autosearch.spec.js

--- cypress/integration/autosearch.spec.js
+++ cypress/integration/autosearch.spec.js
@@ -9,6 +9,7 @@ describe("Test search function", () => {
     cy.get(".product_list").children().should("have.length", 7);
 
     //Why this is not working?
+    //因为find只会找到第一个满足条件的，所以我猜你这句话实际只返回给你1个li？
     //cy.get(".product_list").find('li').should("have.length", 7);
   });
 
